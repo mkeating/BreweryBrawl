714667c64fca225b367d47c8c091a430d7ee8f1f
Heyson <heyson.severino@endurance.com>
2019-01-16 14:24:12 -0500

    Setting up our POST Route

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require('express');
 const app = express();
 const sass = require('node-sass-middleware');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+// Compile our sass
 app.use(sass({
   /* Options */
-  src: __dirname,
-  dest: path.join(__dirname, 'public'),
+  src: __dirname + '/sass',
+  dest: __dirname + '/public/css',
   debug: true,
-  outputStyle: 'compressed'
+  outputStyle: 'compressed',
 }));
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// render our index page
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.listen(3333, function () {
   console.log('Example app listening on port 3333!');
+});
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
 });
