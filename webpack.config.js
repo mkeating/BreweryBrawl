3ba088a7d9dc9fa1f88719cbacf5ff8f361d4857
Jesse Chamberlain <jesse.chamberlain@gmail.com>
2019-02-08 11:14:56 -0500

    adjusted config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,19 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public')
-    }
-};
-
-module: {
-    rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
-        }
-    ];
-}
+        ]
+    }
+};
