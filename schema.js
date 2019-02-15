6b19323518024405c8f46d87eac98e7489889db4
Tyson Sukeforth <tyson.sukeforth@endurance.com>
2019-02-15 16:03:22 -0500

    Implement basic Apollo server. (run npm install for new packages)

---
+++ schema.js
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server-express'
+import userModel from './models'
+
+export const typeDefs = gql`
+  type User {
+    id: ID
+    name: String
+    age: Int
+    email: String
+    friends: [User]
+  }
+
+  type Query {
+    users: [User]
+  }
+`
+
+
+export const resolvers = {
+  Query: {
+    users() {
+      return userModel.list()
+    }
+  }
+}
